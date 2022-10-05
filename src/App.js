a9b77e96e977da5064b4d4e4f743c92294e418ed
EstherWangui <esther.nganga@student.moringaschool.com>
2022-10-05 21:59:37 +0300

    commited changes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import data from './data';
 import List from './List';
 function App() {
   const [people, setPeople] = useState(data);
-
   return (
     <main>
       <section className='container'>
+      <button onClick={() => setPeople([])}>clear all</button>
         <h2 style={{color: "blueviolet"}}>{people.length} Birthdays Today</h2>
-        <List people={people} />
-        <button onClick={() => setPeople([])}>clear all</button>
+        <List people={people}
+              
+         />
       </section>
     </main>
   );
