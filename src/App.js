2a654930416f99c137a4bbfd643db31b70e69041
EstherWangui <esther.nganga@student.moringaschool.com>
2022-10-06 12:33:15 +0300

    changed app js file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
 import React, { useState } from 'react';
+import { useHistory } from "react-router-dom";
 import data from './data';
 import List from './List';
+function Login({ setIsLoggedIn }) {
+  const history = useHistory();
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  function handleChange(e) {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if(formData.username && formData.password){
+      setIsLoggedIn(true);
+      history.push("/");
+    }
+
+
+    // after logging the user in, redirect to the home page!
+  }
 function App() {
   const [people, setPeople] = useState(data);
   return (
@@ -12,8 +37,26 @@ function App() {
               
          />
       </section>
+      <article>
+      <form onSubmit={handleSubmit}>
+      <h1>Login</h1>
+      <input
+        type="text"
+        name="username"
+        value={formData.username}
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        name="password"
+        value={formData.password}
+        onChange={handleChange}
+      />
+      <button type="submit">Login</button>
+    </form>
+      </article>
     </main>
   );
 }
-
+}
 export default App;
